66cf5691fd00b197697b9119750d1b9fd4fe67a5
Pingan Yi <pingany@gmail.com>
2015-02-12 11:37:34 +0800

    sortLinkedList ok, need performance tunning

--- sortLinkedList.py
+++ sortLinkedList.py
@@ -20,28 +20,31 @@ def listTraverse(node, n):
         node = node.next
     return node, prev
 
-def merge(node1, node2, l):
+def listInsert(node, nodeToInsert, maxStep=(1<<30)):
+    if maxStep <= 0 or not nodeToInsert:
+        return node
     head = ListNode(0)
+    head.next = node
     prev = head
+    while maxStep > 0 and node and node.val <= nodeToInsert.val:
+        prev = node
+        node = node.next
+        maxStep -= 1
+    prev.next = nodeToInsert
+    nodeToInsert.next = node
+    return head.next
+
+def merge(node1, node2, l):
+    assert l > 0
     l1 = l
     l2 = l
-    while (node1 and l1) or (node2 and l2):
-        condition1 = node1 and l1
-        condition2 = node2 and l2
-        if condition1 and ((not condition2) or node1.val < node2.val):
-            x = node1
-            node1 = node1.next
-            l1 -= 1
-            assert l1 >= 0
-        else:
-            assert node2
-            x = node2
-            node2 = node2.next
-            l2 -= 1
-            assert l2 >= 0
-        prev.next = x
-        prev = x
-    return head.next
+    while l1 > 0 and node1:
+        node1Next = node1.next
+        node2 = listInsert(node2, node1, l2)
+        l1 -= 1
+        l2 += 1
+        node1 = node1Next
+    return node2
 
 def sortList(node):
     head = ListNode(0)
@@ -50,12 +53,11 @@ def sortList(node):
     l = 1
     while l < len:
         prev = head
-        print "len ", l
+        node = prev.next
+        # assert len == listLen(node)
         while node:
             next, _ = listTraverse(node, l)
             prev.next = merge(node, next, l)
-
-            print listLen(prev.next)
             node, prev = listTraverse(prev.next, l*2)
         l *= 2
     return head.next
@@ -69,7 +71,35 @@ def assertSortOK(list):
     node2 = sortList(node)
     assert nodesToList(node2) == sorted(list)
 
-def test():
+def testInsert():
+    node = listToNodes([1, 2])
+    node = listInsert(node, ListNode(3))
+    assert [1, 2, 3] == nodesToList(node)
+
+    node = listInsert(node, ListNode(0))
+    assert [0, 1, 2, 3] == nodesToList(node)
+
+    node = listInsert(node, ListNode(1.5))
+    assert [0, 1, 1.5, 2, 3] == nodesToList(node)
+
+    node = listInsert(node, ListNode(4), 0)
+    assert [0, 1, 1.5, 2, 3] == nodesToList(node)
+
+    node = listInsert(node, ListNode(4), 1)
+    assert [0, 4, 1, 1.5, 2, 3] == nodesToList(node)
+
+    node = listInsert(node, ListNode(-1), 1)
+    assert [-1, 0, 4, 1, 1.5, 2, 3] == nodesToList(node)
+
+    node = listToNodes([1, 2])
+    assert [1, 2, 2] == nodesToList(listInsert(node, ListNode(2)))
+    assert [1, 2, 2, 3] == nodesToList(listInsert(node, ListNode(3)))
+
+    assert [] == nodesToList(listInsert(None, None))
+    assert [1] == nodesToList(listInsert(ListNode(1), None))
+    assert [1] == nodesToList(listInsert(None, ListNode(1)))
+
+def testMerge():
     node1 = listToNodes([1, 2])
     node2 = listToNodes([2, 3])
     node3 = merge(node1, node2, 10)
@@ -82,19 +112,74 @@ def test():
     assertMergeOK([1], [2], [1, 2])
     assertMergeOK([2], [1], [1, 2])
 
-    assertMergeOK([2], [1], [], 0)
     assertMergeOK([2,3], [1,1], [1, 2], 1)
     assertMergeOK([2,3], [1,1], [1, 1, 2, 3], 2)
+
+    assertMergeOK([2,3], [1], [1, 2, 3], 1000)
 
     assert merge(None, None, 100) == None
     node = ListNode(1)
     assert merge(node, None, 100) == node
     assert merge(None, node, 100) == node
 
+    node = listToNodes([2, 1])
+    sortedNode = merge(node, node.next, 1)
+    assert [1, 2] == nodesToList(sortedNode)
+
+    node = listToNodes([2])
+    sortedNode = merge(node, node.next, 1)
+    assert [2] == nodesToList(sortedNode)
+
+    node = listToNodes([2, 1, 0])
+    sortedNode = merge(node, node.next, 1)
+    assert [1, 2, 0] == nodesToList(sortedNode)
+
+    assert None == merge(None, None, 100)
+    assert None == merge(None, None, 1)
+
+    node = ListNode(2)
+    assert node == merge(None, node, 1)
+    assert node.next == None
+    assert node == merge(node, None, 1)
+    assert node.next == None
+
+    assert [2] == nodesToList(merge(ListNode(2, ListNode(1)), None,1))
+    assert [1, 2] == nodesToList(merge(ListNode(2, ListNode(1)), None,2))
+    assert [1, 2] == nodesToList(merge(ListNode(2, ListNode(1)), None,3))
+
+    node = listToNodes([3, 4, 1, 5, 2])
+    assert [1, 3, 4, 5, 2] == nodesToList(merge(node, listTraverse(node, 2)[0], 2))
+
+    node = listToNodes([3, 4, 1, 5, 2])
+    assert [1, 2, 3, 4, 5] == nodesToList(merge(node, listTraverse(node, 4)[0], 4))
+
+def testTraverse():
+    assert (None, None) == listTraverse(None, 0)
+    assert (None, None) == listTraverse(None, 1)
+    assert (None, None) == listTraverse(None, 100)
+
+    node = ListNode(1)
+    assert (node, node) == listTraverse(node, 0)
+    assert (node.next, node) == listTraverse(node, 1)
+    assert (node.next, node) == listTraverse(node, 1000)
+
+    node = listToNodes([1, 2])
+    assert (node.next, node) == listTraverse(node, 1)
+    assert (node.next.next, node.next) == listTraverse(node, 2)
+    assert (None, node.next) == listTraverse(node, 3)
+    assert (None, node.next) == listTraverse(node, 1000)
+
+def testSort():
+    assertSortOK([2, 1])
+
     assertSortOK([1, 2])
     assertSortOK([1])
     assertSortOK([])
     assertSortOK([1, 2, 1])
-def test2():
-    assertSortOK([2, 1])
+
+    for i in range(100):
+        assertSortOK([randint(1, 1000) for x in range(0, i)])
+
+def testPerformance():
+    assertSortOK([randint(1, 1000) for x in range(1000)])
     pass
